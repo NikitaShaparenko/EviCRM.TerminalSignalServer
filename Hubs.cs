b0e1161c16dfb3b5961d952e353984ce041a6a08
Nikita Shaparenko <nikita4shap@gmail.com>
2022-09-22 12:22:14 +0300

    Первый коммит
    Перенос наработок с TortoiseSVN

---
+++ Hubs.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace EviCRM.TerminalWebServer.Hubs
+{
+    public class StatusHub : Hub
+    {
+        //---------------------------------------------------------------------
+        //                          SignalR StatusHub
+        //---------------------------------------------------------------------
+
+        public async Task SendAll(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendAllExceptMe(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Others.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendBack(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Caller.SendAsync("broadcastMessage", name, message);
+        }
+    }
+
+    public class MainHub : Hub
+    {
+        //---------------------------------------------------------------------
+        //                          SignalR MainHub
+        //---------------------------------------------------------------------
+
+        public async Task SendAll(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendAllExceptMe(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Others.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendBack(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Caller.SendAsync("broadcastMessage", name, message);
+        }
+    }
+    public class DataHub : Hub
+    {
+        //---------------------------------------------------------------------
+        //                          SignalR DataHub
+        //---------------------------------------------------------------------
+
+        public async Task SendAll(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendAllExceptMe(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Others.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendBack(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Caller.SendAsync("broadcastMessage", name, message);
+        }
+    }
+    public class CmdsHub: Hub
+    {
+        //---------------------------------------------------------------------
+        //                          SignalR CmdsHub
+        //---------------------------------------------------------------------
+
+        public async Task SendAll(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendAllExceptMe(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Others.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendBack(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Caller.SendAsync("broadcastMessage", name, message);
+        }
+    }
+    public class GeneralHub : Hub
+    {
+        //---------------------------------------------------------------------
+        //                          SignalR General Operator
+        //---------------------------------------------------------------------
+
+        public async Task SendAll(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendAllExceptMe(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Others.SendAsync("broadcastMessage", name, message);
+        }
+        public async Task SendBack(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.Caller.SendAsync("broadcastMessage", name, message);
+        }
+
+        
+    }
+}
